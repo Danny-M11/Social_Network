5718e79b5a8227d1e33fa4a8f57cc10d9f0f0b75
Danny-M11 <meddane22@gmail.com>
2023-06-02 14:50:35 +0300

    delete console.log

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import store from './redux/reduxStore';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log(process.env.PUBLIC_URL)
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Provider store = {store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
