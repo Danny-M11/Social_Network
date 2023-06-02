8fe2854d1e09fc130f8e3c0739e3184135fc9807
Danny-M11 <meddane22@gmail.com>
2023-06-02 13:47:14 +0300

    change basename

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(process.env.PUBLIC_URL)
 root.render(
-    <BrowserRouter basename="/Social_Network">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store = {store}>
             <App />
         </Provider>
